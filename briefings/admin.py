b1f5ed802d471632e1a9845bda54769c92cf15c1
João Pedro Prado <joaopedro0128@hotmail.com>
2024-04-27 14:07:08 -0300

    feat: models briefings and categories

--- briefings/admin.py
+++ briefings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from briefings import models
+
+
+admin.site.register(models.Category)
+admin.site.register(models.Briefing)
--- briefings/models.py
+++ briefings/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Briefing(models.Model):
+    name = models.CharField(max_length=255)
+    retailer = models.ForeignKey('retailers.Retailer', on_delete=models.RESTRICT)
+    responsible = models.ForeignKey('vendors.Vendor', on_delete=models.RESTRICT)
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+    release_date = models.DateTimeField()
+    available = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.name
