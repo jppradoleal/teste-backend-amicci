b9e4ba3e0f7aa4c74b9b2bebdd31c2d7734e096a
João Pedro Prado <joaopedro0128@hotmail.com>
2024-04-27 22:09:13 -0300

    feat: returns vendors related to retailers

--- retailers/serializers.py
+++ retailers/serializers.py
@@ -1,9 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from retailers import models
+from vendors.models import Vendor
+from vendors.serializers import VendorSerializer
 
+
+class RetailerSerializer(serializers.ModelSerializer):
+    vendors = serializers.SerializerMethodField()
 
     class Meta:
         model = models.Retailer
-        fields = ["id", "name"]
-        read_only_fields = ["id"]
+        fields = ["id", "name", "vendors"]
+        read_only_fields = ["id", "vendors"]
+
+    def get_vendors(self, obj):
+        briefings = obj.briefing_set.all()
+        vendor_ids = briefings.values_list("responsible", flat=True).distinct()
+        vendors = Vendor.objects.filter(id__in=vendor_ids)
+        return VendorSerializer(vendors, many=True).data
