f4748fa248a97849f18aa3c341b8c3f7e371cb92
João Pedro Prado <joaopedro0128@hotmail.com>
2024-04-27 23:38:47 -0300

    feat: adds owner field to models
    Signed-off-by: João Pedro Prado <joaopedro0128@hotmail.com>

--- briefings/models.py
+++ briefings/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from amicci import settings
+
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=500)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, null=True)
 
     class Meta:
         verbose_name_plural = "categories"
@@ -19,6 +22,7 @@ class Briefing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.RESTRICT)
     release_date = models.DateTimeField()
     available = models.BooleanField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, null=True)
 
     def __str__(self) -> str:
         return self.name
--- retailers/models.py
+++ retailers/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from amicci import settings
+
 
 class Retailer(models.Model):
     name = models.CharField(max_length=255)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, null=True)
 
     def __str__(self) -> str:
         return self.name
