c8a739a33f56d485575249f103ca3f89b4cd38de
João Pedro Prado <joaopedro0128@hotmail.com>
2024-04-28 04:25:34 -0300

    fix: check for uniqueness on user email

--- core/serializers.py
+++ core/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from dj_rest_auth.serializers import LoginSerializer
+from rest_framework.validators import UniqueValidator
+
+from core.models import User
 
 
 class CustomRegisterSerializer(RegisterSerializer):
     username = None
+    email = serializers.EmailField(
+        required=True, validators=[UniqueValidator(User.objects.all())]
+    )
     password1 = serializers.CharField(write_only=True, style={"input_type": "password"})
     password2 = serializers.CharField(write_only=True, style={"input_type": "password"})
 
@@ -22,7 +28,8 @@ class CustomRegisterSerializer(RegisterSerializer):
 class CustomLoginSerializer(LoginSerializer):
     username = None
 
-class ModelSerializerWithOwner(serializers.ModelSerializer):    
+
+class ModelSerializerWithOwner(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["owner"] = self.context["request"].user
         return super().create(validated_data)
